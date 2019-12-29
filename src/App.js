30d0d90b538bbb4bf595eb0d329665f3235504dc
ladislasfontaine <fontaine.ladislas@gmail.com>
2019-12-29 22:12:30 +0100

    Save routing.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import React from 'react'
 import './App.css'
 import Nav from './Nav'
 import Pattern from './Pattern'
+import Work from './Work'
 import About from './About'
 import Project from './Project'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App () {
   return (
-    <div className='App'>
-      <Nav />
-      <About />
-      <Project />
-      <Pattern />
-    </div>
+    <Router>
+      <div className='App'>
+        <Nav />
+        <Pattern />
+        <Switch>
+          <Route path='/' exact component={Work} />
+          <Route path='/about' component={About} />
+          <Route path='/project' component={Project} />
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
--- src/Nav.js
+++ src/Nav.js
@@ -1,26 +1,27 @@
 import React from 'react'
 import './App.css'
 import './Nav.css'
+import { Link } from 'react-router-dom'
 
 function Nav () {
   return (
     <nav className='nav'>
       <ul className='nav-left'>
         <li className='logo-li'>
-          <a href='' className='logo'>
+          <Link to='/' className='logo'>
             L
-          </a>
+          </Link>
           <div className='logo-shadow' />
         </li>
         <li>
-          <a href='' className='button home'>
+          <Link to='/' className='button home'>
             Work
-          </a>
+          </Link>
         </li>
         <li>
-          <a href='' className='button about'>
+          <Link to='/about' className='button about'>
             About
-          </a>
+          </Link>
         </li>
       </ul>
       <ul className='nav-right'>
