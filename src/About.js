32becd79ce1e75b3754bdbec7399b51516986ba4
ladislasfontaine <fontaine.ladislas@gmail.com>
2020-01-12 16:55:57 +0100

    About section ok but not responsive.

--- src/About.js
+++ src/About.js
@@ -33,10 +33,57 @@ function About () {
             />
           </div>
           <div className='aboutSectionText'>
-            <h5>Born and raised in</h5>
+            <h5 className='green'>Born and raised in</h5>
             <h4>France</h4>
           </div>
         </div>
+      </div>
+
+      <div className='aboutSection'>
+        <img
+          src={process.env.PUBLIC_URL + '/about/about-bg-2.png'}
+          alt='Man surfing'
+          className='backgroundImage'
+        />
+        <div className='aboutSectionBackground' />
+        <div className='aboutSectionContent'>
+          <div className='smallImageBackground'>
+            <img
+              src={process.env.PUBLIC_URL + '/about/ieseg.png'}
+              alt='Avatar'
+              className='smallImage iesegLogo'
+            />
+          </div>
+          <div className='aboutSectionText'>
+            <h5 className='green'>Graduated from</h5>
+            <h4 className='iesegName'>IESEG Business School</h4>
+          </div>
+        </div>
+      </div>
+
+      <div className='aboutSection'>
+        <img
+          src={process.env.PUBLIC_URL + '/about/about-bg-3.png'}
+          alt='Man surfing'
+          className='backgroundImage'
+        />
+        <div className='aboutSectionBackground lighterBackground' />
+        <div className='aboutSectionContent'>
+          <div className='smallImageBackground'>
+            <img
+              src={process.env.PUBLIC_URL + '/about/ruby.png'}
+              alt='Avatar'
+              className='smallImage'
+            />
+          </div>
+          <div className='aboutSectionText'>
+            <h4>Ruby Lover</h4>
+          </div>
+        </div>
+      </div>
+
+      <div className='lastAboutSection'>
+        <p className='green'>Looking for opportunities.</p>
       </div>
     </div>
   )
