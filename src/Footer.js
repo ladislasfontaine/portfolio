67429902ec3553224dfb4d5d1a23dc7d515329aa
ladislasfontaine <fontaine.ladislas@gmail.com>
2020-01-14 07:07:34 +0100

    Save footer progress.

--- src/Footer.js
+++ src/Footer.js
@@ -3,12 +3,21 @@ import './App.css'
 import './Footer.css'
 import { Link } from 'react-router-dom'
 
-function Footer () {
+function Footer ({ project }) {
   return (
     <div className='Footer'>
-      <h1>
-        Footer Component {this.props.project.id}
-      </h1>
+      <div className='insideFooter'>
+        <Link to={`/project/${project.id - 1}`} className='footerButton'>
+          Prev
+        </Link>
+        <a href='#Top' className='footerTopButton'>
+          <img src={process.env.PUBLIC_URL + '/icon-arrow.svg'} alt='Arrow' />
+          <span>Top</span>
+        </a>
+        <Link to={`/project/${project.id + 1}`} className='footerButton'>
+          Next
+        </Link>
+      </div>
     </div>
   )
 }
