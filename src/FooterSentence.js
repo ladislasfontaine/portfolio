9506792c191dc954b5f1d7f1ce268d8f06fcd824
ladislasfontaine <fontaine.ladislas@gmail.com>
2020-09-24 09:22:37 +0200

    updated npm and footer

--- src/FooterSentence.js
+++ src/FooterSentence.js
@@ -3,7 +3,11 @@ import "./App.css";
 import "./FooterSentence.css";
 
 function FooterSentence() {
-  return <div className="FooterSentence">Made from scratch with React.</div>;
+  return (
+    <div className="FooterSentence">
+      Made from scratch with React | Inspired by <a href="https://www.mrdavidhuynh.com/" target="_blank" rel="noopener noreferrer">David Huynh</a>
+    </div>
+  );
 }
 
 export default FooterSentence;
