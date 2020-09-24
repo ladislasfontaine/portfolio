df93c5764b155bb33da79c0d90cc6cb81a7ad87b
ladislasfontaine <fontaine.ladislas@gmail.com>
2020-09-24 16:41:48 +0200

    Updates

---
+++ precache-manifest.60461540b0b31d3795b3d56c6527716f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b0e5a110120b8417c93c2e80078719c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fd1b4012319ffa24bd8",
+    "url": "/static/css/main.b3e892ee.chunk.css"
+  },
+  {
+    "revision": "fc8ba03232d0f0780017",
+    "url": "/static/js/2.128b0b47.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.128b0b47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fd1b4012319ffa24bd8",
+    "url": "/static/js/main.39c00b1d.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
